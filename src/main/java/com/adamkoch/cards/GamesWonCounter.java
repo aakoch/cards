b0cf0c160899370ff1a96e51bf57993fc04620a2
Adam Koch <adam@dogtired.org>
2017-07-29 16:13:42 -0500

    GamesWonCounter

---
+++ src/main/java/com/adamkoch/cards/GamesWonCounter.java
@@ -0,0 +1,36 @@
+package com.adamkoch.cards;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * <p>Created by aakoch on 2017-07-29.</p>
+ *
+ * @author aakoch
+ * @since 1.0.0
+ */
+public class GamesWonCounter {
+
+    private Map<Player, Integer> map = new ConcurrentHashMap<>();
+    private int totalNumberOfGames;
+
+    public void increment(Player winner) {
+        totalNumberOfGames++;
+        map.putIfAbsent(winner, Integer.valueOf(0));
+        map.computeIfPresent(winner, (player, timesWon) -> Integer.valueOf(timesWon + 1));
+    }
+
+    @Override
+    public String toString() {
+        String stats = map.entrySet().stream()
+                          .sorted(Comparator.comparing((Map.Entry<Player, Integer> entry) -> entry.getValue()).reversed())
+                          .map(entry -> {
+                              final double v = 100.0 * (double) entry.getValue() / (double) totalNumberOfGames;
+                              return entry.getKey().getName() + ": " + String.format("%.2f%%", v);
+                          })
+                          .collect(Collectors.joining(", "));
+        return stats;
+    }
+}
--- src/main/java/com/adamkoch/cards/Main.java
+++ src/main/java/com/adamkoch/cards/Main.java
@@ -16,8 +16,7 @@ private static final Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
         listStandardDeck();
-//        GamesWonCounter gamesWonCounter = new GamesWonCounter();
-        Map<Player, Integer> map = new ConcurrentHashMap<>();
+        GamesWonCounter gamesWonCounter = new GamesWonCounter();
         Players players = new Players(PlayerFactory.initializePlayers(4));
 
         final int totalNumberOfGames = 10000;
@@ -27,20 +26,13 @@ private static final Logger LOGGER = LogManager.getLogger(Main.class);
             final Player winner = game.play();
             LOGGER.info(winner.getName() + " won");
 
-            map.putIfAbsent(winner, Integer.valueOf(0));
-            map.computeIfPresent(winner, (player, timesWon) -> Integer.valueOf(timesWon + 1));
+            gamesWonCounter.increment(winner);
 
             resetPlayers(players);
         }
 
-        String stats = map.entrySet().stream()
-              .sorted(Comparator.comparing((Map.Entry<Player, Integer> entry) -> entry.getValue()) .reversed())
-              .map(entry -> {
-                  final double v = 100.0 * (double) entry.getValue() / (double) totalNumberOfGames;
-                  return entry.getKey().getName() + ": " + String.format("%.2f%%", v);
-              })
-              .collect(Collectors.joining(", "));
-        LOGGER.info(stats);
+
+        LOGGER.info(gamesWonCounter);
     }
 
     private static void resetPlayers(Players players) {
